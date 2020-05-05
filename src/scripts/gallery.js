8869bd019f2a73433a889bf406b1e61a89c2786d
Alf Catlover <alfcatloverr@gmail.com>
2020-05-05 18:37:19 +0300

    Refactor slider centring

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -14,7 +14,7 @@ export default class Gallery {
     this.container = container
     this.setupLayout();
     this.setupEventHandlers();
-    this.setCurrentSlide(this.currentIndex);
+    this.scrollToSlide(this.currentIndex);
   }
 
   getPhotoIndexById(id) {
@@ -34,33 +34,33 @@ export default class Gallery {
 
   setupEventHandlers() {
     this.container.querySelector('.slider-btn-next').addEventListener('click', () => {
-      this.setCurrentSlide(this.currentIndex + 1);
+      this.scrollToSlide(this.currentIndex + 1);
     });
     this.container.querySelector('.slider-btn-prev').addEventListener('click', () => {
-      this.setCurrentSlide(this.currentIndex - 1);
+      this.scrollToSlide(this.currentIndex - 1);
     });
 
     this.container.querySelectorAll('.slide').forEach(($slide) => {
       $slide.addEventListener('click', () => {
-        debugger;
-        this.setCurrentSlide(parseInt($slide.dataset.index));
+        this.scrollToSlide(parseInt($slide.dataset.index));
       })
     })
   }
 
-  setCurrentSlide(currentSlide) {
+  scrollToSlide(currentSlide) {
     if (currentSlide >= 0 && currentSlide < this.photos.length) {
       this.prevSlide = this.currentIndex;
       this.currentIndex = currentSlide;
-
       const $slider = document.querySelector('.slider-in');
-      const maxOffset = this.photos.length * this.slideWidth - (this.size - 1) * this.slideWidth;
-      const offset = (this.slideWidth * -currentSlide) + (this.slideWidth * Math.ceil((this.size - 1) / 2));
+      let offset = (this.slideWidth * -currentSlide) + (this.slideWidth * Math.ceil((this.size - 1) / 2));
 
-      if (offset <= 0 && Math.abs(offset) < maxOffset) {
-        $slider.style.left = `${offset}px`;
+      if (currentSlide <= Math.ceil(this.size / 2)) {
+        offset = 0;
+      } else if (currentSlide >= Math.floor(this.photos.length - this.size / 2)) {//6
+        offset = -(this.photos.length - this.size) * this.slideWidth;
       }
 
+      $slider.style.left = `${offset}px`;
       $slider.querySelector(`.slide:nth-child(${this.prevSlide + 1})`).classList.remove('slide-active');
       $slider.querySelector(`.slide:nth-child(${currentSlide + 1})`).classList.add('slide-active');
     }
